9b56bb8e646d95afb41b7366283f21246e599507
CnybTseng <984943763@qq.com>
2020-04-15 18:03:53 +0800

    Towards Real-Time Multi-Object Tracking

--- kalman.py
+++ kalman.py
@@ -1,7 +1,22 @@
 import cv2
 import numpy as np
+from scipy.spatial.distance import mahalanobis
 
-class KalmanFilter(cv2.KalmanFilter):
+# 取95%置信度时卡方分布上侧alpha分数
+# 同样来自Matlab的chi2inv()函数
+chi2inv95 = {
+    1:3.841459,
+    2:5.991465,
+    3:7.814728,
+    4:9.487729,
+    5:11.070498,
+    6:12.591587,
+    7:14.067140,
+    8:15.507313,
+    9:16.918978
+}
+
+class KalmanFilter(object):
     '''多目标跟踪的卡尔曼滤波器. 状态向量s=(x,y,a,h,vx,vy,va,vh),
         其中的a是目标建议框横纵比. 状态转移函数如下:
         x  = x + vx
@@ -14,48 +29,111 @@ class KalmanFilter(cv2.KalmanFilter):
         vh = vh
     '''
     def __init__(self):
-        super(KalmanFilter, self).__init__(8, 4)
-        self.transitionMatrix = np.eye(8, 8)
+        # Inheritance from cv2.KalmanFilter is not supported
+        # Please reference https://github.com/opencv/opencv/issues/15804
+        self.kf = cv2.KalmanFilter(8, 4)
+        self.kf.transitionMatrix = np.eye(8, 8, dtype=np.float32)
         for i in range(4):
-            self.transitionMatrix[i, 4+i] = 1.0
-        self.measurementMatrix = np.eye(4, 8)
+            self.kf.transitionMatrix[i, 4+i] = 1.0
+        self.kf.measurementMatrix = np.eye(4, 8, dtype=np.float32)
         self.std_weight_position = 1. / 20
         self.std_weight_velocity = 1. / 160
+    
+    def initialize(self, measurement):
+        '''初始化后验状态和后验误差估计协方差
+        
+        Args:
+            measurement (numpy.ndarray): 测量, measurement=[x,y,a,h]
+        '''
+        self.kf.statePost = np.r_[measurement, np.zeros_like(measurement)].astype(np.float32)
+        self.kf.errorCovPost = np.diag(np.square([
+            2  * self.std_weight_position * measurement[3],
+            2  * self.std_weight_position * measurement[3],
+            1e-2,
+            2  * self.std_weight_position * measurement[3],
+            10 * self.std_weight_velocity * measurement[3],
+            10 * self.std_weight_velocity * measurement[3],
+            1e-5,
+            10 * self.std_weight_velocity * measurement[3]
+        ])).astype(np.float32)
     
     def predict(self, *args, **kwargs):
         '''预测
         '''
         
         # 计算过程噪声协方差
-        self.processNoiseCov = np.diag(np.square([
-            self.std_weight_position * self.statePre[3],
-            self.std_weight_position * self.statePre[3],
+        self.kf.processNoiseCov = np.diag(np.square([
+            self.std_weight_position * self.kf.statePre[3],
+            self.std_weight_position * self.kf.statePre[3],
             1e-2,
-            self.std_weight_position * self.statePre[3],
-            self.std_weight_velocity * self.statePre[3],
-            self.std_weight_velocity * self.statePre[3],
+            self.std_weight_position * self.kf.statePre[3],
+            self.std_weight_velocity * self.kf.statePre[3],
+            self.std_weight_velocity * self.kf.statePre[3],
             1e-5,
-            self.std_weight_velocity * self.statePre[3]
-        ]))
+            self.std_weight_velocity * self.kf.statePre[3]
+        ])).astype(np.float32)
         
         # 预测状态和误差估计协方差
-        return super().predict(*args, **kwargs)
+        return self.kf.predict(*args, **kwargs)
     
     def correct(self, measurement):
         '''更新
         '''
         
         # 计算测量噪声协方差
-        self.measurementNoiseCov = np.diag(np.square(
-            self.std_weight_position * self.statePre[3],
-            self.std_weight_position * self.statePre[3],
+        self.kf.measurementNoiseCov = np.diag(np.square([
+            self.std_weight_position * self.kf.statePre[3],
+            self.std_weight_position * self.kf.statePre[3],
             1e-1,
-            self.std_weight_position * self.statePre[3]
-        ))
+            self.std_weight_position * self.kf.statePre[3]
+        ])).astype(np.float32)
         
-        return super().correct(measurement)
+        return self.kf.correct(measurement)
+    
+    def project(self):
+        '''将状态分布投影到测量空间
+        '''
+        measurementNoiseCov = np.diag(np.square([
+            self.std_weight_position * self.kf.statePost[3],
+            self.std_weight_position * self.kf.statePost[3],
+            1e-1,
+            self.std_weight_position * self.kf.statePost[3]
+        ]))
+        
+        mean = np.dot(self.kf.measurementMatrix, self.kf.statePost)
+        covariance = np.linalg.multi_dot((self.kf.measurementMatrix,
+            self.kf.errorCovPost, self.kf.measurementMatrix.T)) + measurementNoiseCov
+        return mean, covariance
+    
+    def gating_distance(self, measurement, only_position=False, metric='maha'):
+        '''计算测量和状态分布之间的门限距离(Mahalanobis distance)
+            Please reference https://en.wikipedia.org/wiki/Mahalanobis_distance
+        
+        Args:
+            measurement (numpy.ndarray): 测量, measurement=[x,y,a,h]
+            only_position (bool, optional): 仅使用位置信息, 如果为真,
+                仅考虑建议框的中心坐标
+            metric (str, optional): 距离度量方法
+        Returns:
+            dists (numpy.ndarray): 测量和状态分布之间的门限距离
+        '''
+        mean, covariance = self.project()
+        # covariance = np.linalg.inv(covariance)
+        # dists = [mahalanobis(x, mean, covariance) for x in measurement]
+        # dists = np.square(dists)
+        d = measurement - mean
+        cholesky_factor = np.linalg.cholesky(covariance)
+        z = scipy.linalg.solve_triangular(
+                cholesky_factor, d.T, lower=True, check_finite=False,
+                overwrite_b=True)
+        dists = np.sum(z * z, axis=0)
+        return dists
 
 if __name__ == '__main__':
-    kf = KalmanFilter()
-    print(kf.transitionMatrix)
-    print(kf.measurementMatrix)
+    kfs = [KalmanFilter() for _ in range(50)]
+    print(kfs)
+    print(kfs[0].kf.transitionMatrix)
+    print(kfs[0].kf.measurementMatrix)
+    
+    measurement = np.load('measurements.npy')
+    dists = kfs[0].gating_distance(measurement)
--- tracker.py
+++ tracker.py
@@ -1,11 +1,16 @@
 import os
 import re
 import cv2
+import lap
 import torch
 import argparse
 import numpy as np
+from enum import IntEnum
 from torchvision.ops import nms
+from cython_bbox import bbox_overlaps
+from scipy.spatial.distance import cdist
 
+import kalman
 import yolov3
 import darknet
 import dataset
@@ -23,6 +28,7 @@ def parse_args():
 
 def xywh2tlbr(boxes):
     '''转换建议框的数据格式
+    
     Args:
         boxes (torch.Tensor): [x,y,w,h]格式的建议框
     Returns:
@@ -34,6 +40,25 @@ def xywh2tlbr(boxes):
     tlbr[:,2] = boxes[:,0] + boxes[:,2]/2
     tlbr[:,3] = boxes[:,1] + boxes[:,3]/2
     return tlbr
+
+def tlbr2xyah(tlbr):
+    '''转换建议框的数据格式
+    
+    Args:
+        tlbr (numpy.ndarray): [t,l,b,r]格式的建议框
+    Returns:
+        xyah (numpy.ndarray): [x,y,a,h]格式的建议框
+    '''
+    dim = len(tlbr.shape)
+    tlbr = tlbr.reshape(-1, 4)
+    xyah = np.zeros_like(tlbr)
+    xyah[:,0] = (tlbr[:,1] + tlbr[:,3]) / 2
+    xyah[:,1] = (tlbr[:,0] + tlbr[:,2]) / 2
+    xyah[:,3] = (tlbr[:,2] - tlbr[:,0])
+    xyah[:,2] = (tlbr[:,3] - tlbr[:,1]) / xyah[:,3]
+    if dim == 1:
+        xyah = xyah.reshape(-1)
+    return xyah
 
 def nonmax_suppression(dets, score_thresh=0.5, iou_thresh=0.4):
     '''检测器输出的非最大值抑制
@@ -94,6 +119,361 @@ def overlap(dets, im):
         cv2.rectangle(im, (t, l), (b, r), color, 2)
     return im
 
+class TrajectoryState(IntEnum):
+    New = 0
+    Tracked = 1
+    Lost = 2
+    Removed = 3
+
+class Trajectory(kalman.KalmanFilter):
+    '''轨迹
+    '''
+    count = 0
+    def __init__(self, tlbr, score, embedding):
+        super(Trajectory, self).__init__()
+        self.tlbr = tlbr
+        self.xyah = tlbr2xyah(tlbr)
+        self.score = score
+        self.smooth_embedding = None
+        self.id = 0
+        self.is_activated = False
+        self.eta = 0.9
+        self.timestamp = 0
+        self.length = 0
+        self.starttime = 0
+        self.update_embedding(embedding)
+    
+    def update_embedding(self, embedding):
+        self.current_embedding = embedding / np.linalg.norm(embedding)
+        if self.smooth_embedding is None:
+            self.smooth_embedding = self.current_embedding
+        else:
+            self.smooth_embedding = self.eta * self.smooth_embedding + \
+                (1 - self.eta) * self.current_embedding
+        self.smooth_embedding = self.smooth_embedding / np.linalg.norm(self.smooth_embedding)
+    
+    @staticmethod
+    def next_id():
+        Trajectory.count += 1
+        return Trajectory.count
+    
+    def predict(self, *args, **kwargs):
+        if self.state != TrajectoryState.Tracked:
+            self.kf.statePost[7] = 0
+        return super().predict(*args, **kwargs)
+    
+    def update(self, trajectory, timestamp, update_embedding=True):
+        self.timestamp = timestamp
+        self.length += 1        
+        super().correct(trajectory.xyah)
+        self.state = TrajectoryState.Tracked
+        self.is_activated = True
+        self.score = trajectory.score
+        if update_embedding:
+            self.update_embedding(trajectory.current_embedding)
+    
+    def activate(self, timestamp):
+        self.id = self.next_id()
+        super().initialize(self.xyah)
+        self.length = 0
+        self.state = TrajectoryState.Tracked
+        self.timestamp = timestamp
+        self.starttime = timestamp
+    
+    def reactivate(self, trajectory, timestamp, newid=False):
+        super().correct(trajectory.xyah)
+        self.update_embedding(trajectory.current_embedding)
+        self.length = 0
+        self.state = TrajectoryState.Tracked
+        self.is_activated = True
+        self.timestamp = timestamp
+        if newid:
+            self.id = self.next_id()
+    
+    def mark_lost(self):
+        self.state = TrajectoryState.Lost
+    
+    def mark_removed(self):
+        self.state = TrajectoryState.Removed
+    
+    def timestamp(self):
+        return self.timestamp
+
+def joint_trajectories(A, B):
+    '''合并两个轨迹组
+    
+    Args:
+        A (list of Trajectory): 轨迹组A
+        B (list of Trajectory): 轨迹组B
+    Returns:
+        C (list of Trajectory): 合并后的轨迹组
+    '''
+    exists = {}
+    C = []
+    for t in A:
+        exists[t.id] = 1
+        C.append(t)
+    for t in B:
+        if not exists.get(t.id, 0):
+            exists[t.id] = 1
+            C.append(t)
+    return C
+
+def exclude_trajectories(A, B):
+    '''从轨迹组A中排除那些也在轨迹组B中的轨迹
+    
+    Args:
+        A (list of Trajectory): 轨迹组A
+        B (list of Trajectory): 轨迹组B
+    Returns:
+        C (list of Trajectory): 排除共同轨迹之后的轨迹组A
+    '''
+    exists = {}
+    C = []
+    for t in B:
+        exists[t.id] = 1
+    for t in A:
+        if not exists.get(t.id, 0):
+            C.append(t)
+    return C
+
+def embedding_distance(A, B):
+    '''计算表观嵌入之间的代价矩阵
+    
+    Args:
+        A (list of Trajectory): 轨迹组A
+        B (list of Trajectory): 轨迹组B
+    Returns:
+        costs (numpy.ndarray): 代价矩阵
+    '''
+    costs = np.zeros((len(A), len(B)))
+    if costs.size == 0:
+        return costs
+    XA = np.asarray([trajectory.current_embedding for trajectory in A])
+    XB = np.asarray([trajectory.current_embedding for trajectory in B])
+    costs = np.maximum(0, cdist(XA, XB))
+    return costs
+
+def iou_distance(A, B):
+    '''计算轨迹之间的IOU距离
+    
+    Args:
+        A (list of Trajectory): 轨迹组A
+        B (list of Trajectory): 轨迹组B
+    Returns:
+        costs (numpy.ndarray): 代价矩阵
+    '''
+    BA = [a.tlbr for a in A]
+    BB = [b.tlbr for b in B]
+    ious = np.zeros((len(A), len(B)), dtype=np.float)
+    if ious.size == 0:
+        return ious   
+    ious = bbox_overlaps(np.ascontiguousarray(BA, dtype=np.float),
+        np.ascontiguousarray(BB, dtype=np.float))
+    costs = 1 - ious
+    return costs
+
+def remove_duplicate_trajectories(A, B, thresh=0.15):
+    '''从轨迹组A和轨迹组B中那些重叠的轨迹
+    
+    Args:
+        A (list of Trajectory): 轨迹组A
+        B (list of Trajectory): 轨迹组B
+    Returns:
+        (A,B) (tuple of list of Trajectory): 排除共同轨迹之后的轨迹组A和轨迹组B
+    '''
+    dists = iou_distance(A, B)
+    rows, cols = np.where(dists<thresh)
+    DA, DB = [], []
+    for r, c in zip(rows, cols):
+        ta = A[r].timestamp - A[r].starttime
+        tb = B[c].timestamp - B[c].starttime
+        if ta > tb:
+            DB.append(c)
+        else:
+            DA.append(r)
+    A = [a for i,a in enumerate(A) if not i in DA]
+    B = [b for i,b in enumerate(B) if not i in DB]
+    return A, B
+
+def fuse_motion(trajectory_pool, candidates, dists, lamb=0.98):
+    '''融合表观嵌入距离和分布测量距离
+    
+    Args:
+        trajectory_pool (list of Trajectory): 轨迹池
+        candidates (list of Trajectory): 候选轨迹
+        dists (numpy.ndarray): 轨迹池和候选轨迹之间的代价矩阵
+        lamb (float, optional): 融合两种距离的权重系数
+    Returns:
+        dists (numpy.ndarray): 轨迹池和候选轨迹之间的代价矩阵
+    '''
+    if dists.size == 0:
+        return dists
+    gate_thresh = kalman.chi2inv95[4]
+    measurements = np.asarray([candidate.xyah for candidate in candidates])
+    for i,trajectory in enumerate(trajectory_pool):
+        gdist = trajectory.gating_distance(measurements)
+        dists[i, gdist > gate_thresh] = np.inf
+        dists[i] = lamb * dists[i] + (1 - lamb) * gdist
+
+def linear_assignment(cost, cost_limit):
+    '''线性分配
+    
+    Args:
+        cost (numpy.ndarray): 代价矩阵
+        cost_limit (float): 线性分配代价上限
+    Returns:
+        matches (numpy.ndarray): 一个N*2的匹配索引矩阵, N为匹配对数
+            matches[i,j]表示第j个候选轨迹分配给第i个轨迹池轨迹
+        mismatch_row (numpy.ndarray): 没有分配的行(轨迹池中的轨迹)
+        mismatch_col (numpy.ndarray): 没有分配的列(候选集中的轨迹)
+    '''
+    matches = np.empty((0, 2), dtype=int)
+    mismatch_row = np.arange(cost.shape[0], dtype=int)
+    mismatch_col = np.arange(cost.shape[1], dtype=int)
+    if cost.size == 0:
+        return matches, mismatch_row, mismatch_col
+    
+    matches = []
+    opt, x, y = lap.lapjv(cost, extend_cost=True, cost_limit=cost_limit)
+    for i,xi in enumerate(x):
+        if xi >= 0:
+            matches.append([i, xi])
+    
+    matches = np.asarray(matches)
+    mismatch_row = np.where(x < 0)[0]
+    mismatch_col = np.where(y < 0)[0]
+    return matches, mismatch_row, mismatch_col
+
+class JDETracker(object):
+    '''联合检测和嵌入的目标跟踪器
+    '''
+    def __init__(self):
+        self.timestamp = 0
+        self.tracked_trajectories = []
+        self.lost_trajectories    = []
+        self.removed_trajectories = []
+        self.max_lost_time = 30
+    
+    def update(self, dets):
+        '''根据检测结果更新跟踪器
+        
+        Args:
+            dets (numpy.ndarray): 含检测结果的二维数组, dets[:]=
+            [t,l,b,r,objecness,class,embedding]
+        '''
+        
+        self.timestamp += 1
+        
+        # 用检测结果初始化候选轨迹, 候选轨迹要么融入轨迹池中已有轨迹,
+        # 要么成为新的轨迹加入轨迹池
+        candidates = []
+        for det in dets:
+            candidates += [Trajectory(det[:4], det[4], det[6:])]
+        
+        # 构造轨迹池
+        tracked_trajectories = []
+        unconfirmed_trajectories = []
+        for trajectory in self.tracked_trajectories:
+            if trajectory.is_activated:
+                tracked_trajectories.append(trajectory)
+            else:
+                unconfirmed_trajectories.append(trajectory)
+        
+        trajectory_pool = joint_trajectories(tracked_trajectories, self.lost_trajectories)
+        
+        # 预测轨迹池中轨迹在当前帧的状态
+        for trajectory in trajectory_pool:
+            trajectory.predict()
+        
+        # 根据表观嵌入关联候选轨迹和轨迹库
+        dists = embedding_distance(trajectory_pool, candidates)
+        dists = fuse_motion(trajectory_pool, candidates, dists)
+        matches, mismatch_row, mismatch_col = linear_assignment(dists, cost_limit=0.7)
+        
+        activated_trajectories = []
+        retrieved_trajectories = []
+        for pid, cid in matches:
+            pool_trajectory = trajectory_pool[pid]
+            cand_trajectory = candidates[cid]
+            if pool_trajectory.state == TrajectoryState.Tracked:
+                # 如果池中的轨迹已激活, 将候选轨迹融入轨迹池
+                pool_trajectory.update(cand_trajectory, self.timestamp)
+                activated_trajectories.append(pool_trajectory)
+            else:
+                # 如果池中的轨迹处于休眠状态, 重新激活它
+                pool_trajectory.reactivate(cand_trajectory, self.timestamp)
+                retrieved_trajectories.append(pool_trajectory)
+        
+        # 根据IoU关联候选轨迹和轨迹库
+        candidates = [candidates[i] for i in mismatch_col]
+        # 轨迹池中一直跟踪到上一帧, 但当前帧没有匹配轨迹的那些轨迹
+        mismatch_tracked_pool_trajectories = [trajectory_pool[i] \
+            for i in mismatch_row if trajectory_pool[i].state == TrajectoryState.Tracked]
+        dists = iou_distance(mismatch_tracked_pool_trajectories, candidates)
+        matches, mismatch_row, mismatch_col = linear_assignment(dists, cost_limit=0.5)
+        
+        for pid, cid in matches:
+            pool_trajectory = mismatch_tracked_pool_trajectories[pid]
+            cand_trajectory = candidates[cid]
+            if pool_trajectory.state == TrajectoryState.Tracked:
+                pool_trajectory.update(cand_trajectory, self.timestamp)
+                activated_trajectories.append(pool_trajectory)
+            else:
+                pool_trajectory.reactivate(cand_trajectory, self.timestamp)
+                retrieved_trajectories.append(pool_trajectory)
+        
+        # 轨迹池中跟丢的轨迹
+        lost_trajectories = []
+        for i in mismatch_row:
+            pool_trajectory = mismatch_tracked_pool_trajectories[i]
+            if not pool_trajectory.state == TrajectoryState.Lost:
+                pool_trajectory.mark_lost()
+                lost_trajectories.append(pool_trajectory)
+        
+        # 跟踪过的轨迹中不确定状态的轨迹
+        candidates = [candidates[i] for i in mismatch_col]
+        dists = iou_distance(unconfirmed_trajectories, candidates)
+        matches, mismatch_row, mismatch_col = linear_assignment(dists, cost_limit=0.7)
+        for pid, cid in matches:
+            unconfirmed_trajectories[pid].update(candidates[cid], self.timestamp)
+            activated_trajectories.append(unconfirmed_trajectories[pid])
+        
+        # 跟踪过的不确定状态的轨迹中完全没有匹配上的轨迹
+        removed_trajectories = []
+        for i in mismatch_row:
+            unconfirmed_trajectories[i].mark_removed()
+            removed_trajectories.append(unconfirmed_trajectories[i])
+        
+        # 名花无主的候选轨迹转正, 加入轨迹库
+        for i in mismatch_col:
+            candidates[i].activate(self.timestamp)
+            activated_trajectories.append(candidates[i])
+        
+        # 标记那些跟丢时间过长的轨迹
+        for trajectory in self.lost_trajectories:
+            if self.timestamp - trajectory.timestamp > self.max_lost_time:
+                trajectory.mark_removed()
+                removed_trajectories.append(trajectory)
+        
+        # 更新跟踪过的轨迹列表
+        self.tracked_trajectories = [t for t in self.tracked_trajectories \
+            if t.state == TrajectoryState.Tracked]
+        self.tracked_trajectories = joint_trajectories(self.tracked_trajectories, activated_trajectories)
+        self.tracked_trajectories = joint_trajectories(self.tracked_trajectories, retrieved_trajectories)
+        
+        # 更新跟丢的轨迹列表
+        self.lost_trajectories = exclude_trajectories(self.lost_trajectories, self.tracked_trajectories)
+        self.lost_trajectories.extend(lost_trajectories)
+        self.lost_trajectories = exclude_trajectories(self.lost_trajectories, self.removed_trajectories)
+        
+        # 更新移除的轨迹列表
+        self.removed_trajectories.extend(removed_trajectories)
+        self.tracked_trajectories, self.lost_trajectories = remove_duplicate_trajectories(\
+            self.tracked_trajectories, self.lost_trajectories)
+        
+        return [trajectory for trajectory in self.tracked_trajectories if trajectory.is_activated]
+
 def main(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = darknet.DarkNet().to(device)
@@ -115,7 +495,9 @@ def main(args):
 
     h, w = [int(s) for s in args.insize.split('x')]
     decoder = yolov3.YOLOv3EvalDecoder((h,w), 1, anchors)
+    tracker = JDETracker()
     dataloader = dataset.ImagesLoader(args.img_path, (h,w,3))
+    
     for path, im, lb_im in dataloader:
         input = torch.from_numpy(lb_im).unsqueeze(0).to(device)
         with torch.no_grad():
@@ -125,6 +507,7 @@ def main(args):
         outputs =  nonmax_suppression(outputs)[0]
         print('{}'.format(outputs.size()))
         outputs[:, :4] = tlbr_net2img(outputs[:, :4], (h,w), im.shape[:2])
+        tracker.update(outputs.numpy())
         result = overlap(outputs, im)
         segments = re.split(r'[\\, /]', path)
         cv2.imwrite('result/{}'.format(segments[-1]), result)
